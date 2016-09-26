ff665ca34e57b4bd66810e908a8499f01fd98986
EgorSalenko <wrongua@gmail.com>
2016-09-26 17:12:39 +0300

    Human Weight on Moon task

--- src/main/java/org/esalenko/tasks/HumanWeightOnTheMoon.java
+++ src/main/java/org/esalenko/tasks/HumanWeightOnTheMoon.java
@@ -2,6 +2,11 @@ package org.esalenko.tasks;
 
 public class HumanWeightOnTheMoon {
 
+    /**
+     * Calculate human weight on the Moon
+     * @param weight on Earth
+     * @return weight on the Moon
+     */
     public float calculate(float weight) {
 
         float gravitationalAcceleration = 1.622f;
