2a1f873b248ed0d63278348baa2c0f87846f8b36
EgorSalenko <wrongua@gmail.com>
2016-11-14 23:44:24 +0200

    Queue - tests passed - implementation finished

--- src/test/java/org/esalenko/datastructures/Queue/MyQueueImplTest.java
+++ src/test/java/org/esalenko/datastructures/Queue/MyQueueImplTest.java
@@ -24,17 +24,40 @@ public class MyQueueImplTest {
 
     @Test
     public void dequeue() throws Exception {
+        queue.enqueue(2);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(3);
+        queue.enqueue(6);
 
+        Integer actual = queue.dequeue();
+        Integer expected = 2;
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void peek() throws Exception {
+        queue.enqueue(51);
+        queue.enqueue(3);
+        queue.enqueue(2);
+        queue.enqueue(1);
 
+        Integer actual = queue.peek();
+        Integer expected = 51;
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void size() throws Exception {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
 
+        int actual = queue.size();
+        int expected = 5;
+        Assert.assertEquals(expected, actual);
     }
 
 }
