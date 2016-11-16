fb0245cb4702602ea5fc7dd989304fa88d80c2e5
EgorSalenko <wrongua@gmail.com>
2016-11-16 16:25:49 +0200

    Prototype pattern implementation example

---
+++ src/main/java/org/esalenko/designpatterns/prototype/Copyable.java
@@ -0,0 +1,5 @@
+package org.esalenko.designpatterns.prototype;
+
+public interface Copyable {
+    Object copy();
+}
---
+++ src/main/java/org/esalenko/designpatterns/prototype/ProjectFactory.java
@@ -0,0 +1,17 @@
+package org.esalenko.designpatterns.prototype;
+
+public class ProjectFactory {
+    private Project project;
+
+    public ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    Project cloneProject() {
+        return (Project) project.copy();
+    }
+}
