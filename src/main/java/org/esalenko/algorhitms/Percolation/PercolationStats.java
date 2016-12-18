defee133c1326fcca9b1012c22e2f20ed09c8320
EgorSalenko <wrongua@gmail.com>
2016-12-18 23:08:20 +0200

    PercolationStats algorithm - Base API | Submission Week 1| Coursera course Algorithms 4th Princeton University

---
+++ src/main/java/org/esalenko/algorhitms/Percolation/PercolationStats.java
@@ -0,0 +1,49 @@
+package org.esalenko.algorhitms.Percolation;
+
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class PercolationStats {
+
+    /**
+     * perform trials independent experiments on an n-by-n grid
+     *
+     * @param n
+     * @param trials
+     */
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    /**
+     * @return sample mean of percolation threshold
+     */
+    public double mean() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return low  endpoint of 95% confidence interval
+     */
+    public double confidenceLo() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return high endpoint of 95% confidence interval
+     */
+    public double confidenceHi() {
+        throw new NotImplementedException();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
