2245983c3a8c7ba9a0d45db0be1eaf0a8707ca01
EgorSalenko <wrongua@gmail.com>
2016-12-18 22:27:59 +0200

    Percolation algorithm - v1.0 (worked) | Submission Week 1| Coursera course Algorithms 4th Princeton University

--- src/main/java/org/esalenko/algorhitms/Percolation/Percolation.java
+++ src/main/java/org/esalenko/algorhitms/Percolation/Percolation.java
@@ -4,9 +4,10 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private boolean[][] open;
-    private boolean[][] full;
+    private int[][] sites; // 0 - blocked, 1 - open
     private WeightedQuickUnionUF uf;
+    private int top;
+    private int bottom;
     private int n;
 
     public Percolation(int n) {
@@ -14,68 +15,42 @@ public class Percolation {
         if (n <= 0)
             throw new IllegalArgumentException();
 
-        open = new boolean[n][n];
-        uf = new WeightedQuickUnionUF(n * n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                open[i][j] = false;
-                full[i][j] = false;
-            }
-        }
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        sites = new int[n][n];
+        top = sites[0][0];
+        bottom = sites[n - 1][n - 1];
     }
 
     public void open(int row, int col) {
         validate(row, col);
         if (!isOpen(row, col)) {
-            open[row][col] = true;
-            full = fill(open);
+            sites[row - 1][col - 1] = 1;
+
         }
     }
 
     public boolean isOpen(int row, int col) {
-        return open[row][col];
+        validate(row, col);
+        return sites[row - 1][col - 1] == 1;
     }
 
     public boolean isFull(int row, int col) {
-        return full[row][col];
+        validate(row, col);
+        return uf.connected(top, sites[row - 1][col - 1]);
     }
 
     public boolean percolates() {
-        int n = open.length;
-        for (int i = 0; i < n; i++) {
-            if (full[n - 1][i]) return true;
-        }
-
-        return false;
+        return uf.connected(top, bottom);
     }
 
     private void validate(int row, int col) {
-        if (row <= 0 || row <= n || col <= 0 || col <= n)
+        if (row < 0 || row > n || col < 0 || col > n)
             throw new IndexOutOfBoundsException();
     }
 
-    private boolean[][] fill(boolean[][] open) {
-        for (int i = 0; i < open.length; i++) {
-            flow(open, full, 0, i);
-        }
-        return full;
+    private int getIndex(int row, int col) {
+        return n * (row - 1) + col;
     }
-
-    private void flow(boolean[][] open, boolean[][] full, int row, int col) {
-        int n = open.length;
-
-        if (row < 0 || n < row) return;
-        if (col < 0 || n < col) return;
-        if (isOpen(row, col)) return;
-        if (isFull(row, col)) return;
-
-        flow(open, full, row + 1, col); // up
-        flow(open, full, row - 1, col); // down
-        flow(open, full, row, col + 1); // right
-        flow(open, full, row, col - 1); // left
-    }
-
 
 }
 
