ed7617800e466e7409dfbb8337390acedb48f6ee
EgorSalenko <wrongua@gmail.com>
2016-12-19 22:34:44 +0200

    PercolationStats algorithm - v1.0 | Submission Week 1| Coursera course Algorithms 4th Princeton University

--- src/main/java/org/esalenko/algorhitms/Percolation/PercolationStats.java
+++ src/main/java/org/esalenko/algorhitms/Percolation/PercolationStats.java
@@ -1,49 +1,59 @@
 package org.esalenko.algorhitms.Percolation;
 
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    /**
-     * perform trials independent experiments on an n-by-n grid
-     *
-     * @param n
-     * @param trials
-     */
+    private double[] results;
+    private int trials;
+    private static final double INTERVAL = 1.96D;
+
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException();
 
+        Percolation percolation = new Percolation(n);
+
+        this.trials = trials;
+        results = new double[trials];
+        int opened = 0;
+
+        for (int i = 0; i < trials; i++) {
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(n) + 1;
+                int col = StdRandom.uniform(n) + 1;
+                if (!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                    opened++;
+                }
+            }
+            results[i] = opened / (double) (n * n);
+        }
     }
 
-    /**
-     * @return sample mean of percolation threshold
-     */
+
     public double mean() {
-        throw new NotImplementedException();
+        return StdStats.mean(results);
     }
 
-    /**
-     * @return sample standard deviation of percolation threshold
-     */
     public double stddev() {
-        throw new NotImplementedException();
+        return StdStats.stddev(results);
     }
 
-    /**
-     * @return low  endpoint of 95% confidence interval
-     */
     public double confidenceLo() {
-        throw new NotImplementedException();
+        return mean() - (INTERVAL * stddev() / Math.sqrt(trials));
     }
 
-    /**
-     * @return high endpoint of 95% confidence interval
-     */
     public double confidenceHi() {
-        throw new NotImplementedException();
+        return mean() + (INTERVAL * stddev() / Math.sqrt(trials));
     }
 
     public static void main(String[] args) {
-
+        PercolationStats stats = new PercolationStats(2, 10000);
+        System.out.println("mean = " + stats.mean());
+        System.out.println("std deviation = " + stats.stddev());
+        System.out.println("95% confidence INTERVAL = " + stats.confidenceLo() + ", " + stats.confidenceHi());
     }
 }
