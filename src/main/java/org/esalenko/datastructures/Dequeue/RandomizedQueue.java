6da9eaffd52d656b4267133ea9e04b2c2224d222
EgorSalenko <wrongua@gmail.com>
2017-01-07 11:39:42 +0200

    Randomized Queue base API

---
+++ src/main/java/org/esalenko/datastructures/Dequeue/RandomizedQueue.java
@@ -0,0 +1,48 @@
+package org.esalenko.datastructures.Dequeue;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+
+    }
+
+    // is the queue empty?
+    public boolean isEmpty() {
+        throw new NotImplementedException();
+    }
+
+    // return the number of items on the queue
+    public int size() {
+        throw new NotImplementedException();
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+        throw new NotImplementedException();
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+        throw new NotImplementedException();
+    }
+
+    // return (but do not remove) a random item
+    public Item sample() {
+        throw new NotImplementedException();
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        throw new NotImplementedException();
+    }
+
+    // unit testing (optional)
+    public static void main(String[] args) {
+
+    }
+}
