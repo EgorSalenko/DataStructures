8c18e2c4c23c73c0ac75c77fe47ce6b8eb677a4e
Egor Salenko <wrongua@gmail.com>
2018-01-14 17:12:20 +0200

    Add LineSegment class

---
+++ src/main/java/org/esalenko/algorhitms/Sort/collinear_points/LineSegment.java
@@ -0,0 +1,34 @@
+package org.esalenko.algorhitms.Sort.collinear_points;
+
+
+public class LineSegment {
+
+    private final Point p;   // one endpoint of this line segment
+    private final Point q;   // the other endpoint of this line segment
+
+    /**
+     * Initializes a new line segment.
+     *
+     * @param p one endpoint
+     * @param q the other endpoint
+     * @throws NullPointerException if either <tt>p</tt> or <tt>q</tt>
+     *                              is <tt>null</tt>
+     */
+    public LineSegment(Point p, Point q) {
+        if (p == null || q == null) {
+            throw new NullPointerException("argument is null");
+        }
+        this.p = p;
+        this.q = q;
+    }
+
+
+    public void draw() {
+        p.drawTo(q);
+    }
+
+    public String toString() {
+        return p + " -> " + q;
+    }
+
+}
